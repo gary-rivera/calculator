2606a2fc2af31e049ed32d7a718123ee5de24c0a
gary-rivera <a.gary.rivera@gmail.com>
2024-10-07 20:21:41 -0700

    render ref image

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,29 @@ import './App.css';
 import Calculator from './Calculator.jsx';
 
 function App() {
+	const [refImageRendered, setRefImageRendered] = useState(false);
 	return (
 		<div className="App">
-			<Calculator />
+			<div
+				className={`calculators-container ${
+					refImageRendered ? 'refImageRendered' : ''
+				}`}
+			>
+				<Calculator />
+
+				<div className="calculator-ref-container">
+					<img
+						className="calculator-ref-img"
+						src="/assets/calculator-reference-image.png"
+						alt="Calculator reference"
+					/>
+				</div>
+			</div>
+			<div className="render-controller">
+				<button onClick={() => setRefImageRendered(!refImageRendered)}>
+					Render reference image
+				</button>
+			</div>
 		</div>
 	);
 }
